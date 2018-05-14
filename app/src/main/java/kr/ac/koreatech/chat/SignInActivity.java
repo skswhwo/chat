81db1bf0a838295cdefc3bfe52cdd9e95786a5f0
George <skswhwo@gmail.com>
2018-05-15 01:18:30 +0900

    chore(*): initial state

---
+++ app/src/main/java/kr/ac/koreatech/chat/SignInActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.koreatech.chat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SignInActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
